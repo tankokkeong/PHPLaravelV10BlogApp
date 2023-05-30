030972b6768681351e7f49b85454b356e3baba81
TAN KOK KEONG <tankokkeong4488@gmail.com>
2023-05-31 00:34:04 +0800

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap -->
+<link rel="stylesheet" href="bootstrap-4.5.3-dist/css/bootstrap.min.css">
+<script src="bootstrap-4.5.3-dist/js/jquery-3.5.1.slim.min.js"></script>
+<script src="bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js"></script>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    @extends('layouts.header')
+    <link rel="stylesheet" href={{ Vite::asset('resources/css/login.css') }}>
+</head>
+<body>
+    <div id="login-container">
+        <div class="login-box">
+            <div class="login-title mt-2">
+                <h1 class="text-center">Login</h1>
+
+                <div class="input-container">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="email" onkeyup="enterSignIn()">
+                    </div>
+
+                    <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="password" onkeyup="enterSignIn()">
+                    No account? <a href="signup">Sign Up</a> now!
+                    </div>
+                    
+                    <div class="form-group" id="error-prompt" style="color:red;"></div>
+
+                    <div class="form-group mt-3">
+                    <button type="submit" class="btn btn-primary" onclick="login()">Login</button>
+
+                    <div class="text-info" id="login-loader" role="status" style="display: none;">
+                        <span class="mt-3 ml-1 spinner-border spinner-border-sm"></span>
+                        Loading...
+                    </div>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
